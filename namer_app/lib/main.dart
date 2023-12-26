dfc39663ed7313a9ccc8f03db3b7e0e502b98886
haidun.lhd <kaiton.ri@gmail.com>
2023-12-26 14:41:21 -0400

    Refactor MyHomePage to use NavigationRail

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -50,45 +50,105 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for index $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(children: [
+        SafeArea(
+          child: NavigationRail(
+            extended: false,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite),
+                label: Text('Favorites'),
+              ),
+            ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) {
+              print('selected: $value');
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
     var btnCounter = appState.btnCounter;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            HintTitle(),
-            BigCard(pair: pair),
-            SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // print('button pressed!');
-                    appState.toggleFavorite();
-                  },
-                  label: Text('Like'),
-                  icon: Icon(Icons.favorite),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // print('button pressed!');
-                    appState.getNext();
-                    appState.incrementBtnCounter();
-                  },
-                  child: Text('Next, Btn pressed: $btnCounter'),
-                ),
-              ],
-            ),
-          ],
-        ),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          HintTitle(),
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                  appState.incrementBtnCounter();
+                },
+                child: Text('Next, Btn pressed: $btnCounter'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -130,7 +190,7 @@ class BigCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(
-          pair.asLowerCase,
+          pair.asCamelCase,
           style: style,
           semanticsLabel: "{pair.first} {pair.second}",
         ),
